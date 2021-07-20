8867684d7070855ee9c36ca75d4b5b3f67ea4185
yhy940806 <yhy940806@163.com>
2021-07-19 23:21:44 -0400

    create a request

--- worldmeters/spiders/countries.py
+++ worldmeters/spiders/countries.py
@@ -4,7 +4,7 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
@@ -12,8 +12,8 @@ class CountriesSpider(scrapy.Spider):
         for country in countries:
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
-
-            yield {
-                'country_name': name,
-                'country_link': link
-            }
+            
+            # absolute_url = f"https://www.worldometers.info{link}"
+            absolute_url = response.urljoin(link)
+            
+            yield response.follow(url=link)
